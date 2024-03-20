01e6c9293c33fe5f4a7d36b816730f34b504de9f
Zalaras <damianhunter@gmail.com>
2024-03-20 13:43:30 +0200

    fix: add missing tags

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ func newFunc() {
 }
 
 type Message struct {
-	Header string
-	Body   string
-	Meta   Meta
+	Header string `json:"header"`
+	Body   string `json:"body"`
+	Meta   Meta   `json:"meta"`
 }
 
 type Meta struct {
